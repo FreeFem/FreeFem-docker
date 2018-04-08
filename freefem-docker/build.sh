5ff29bc60cbcd5b0db42ce2adbd5858f1a9f8af0
Gilles <gilles.rasigade+github@gmail.com>
2018-04-08 18:53:37 +0200

    Minor improvements

--- freefem-docker/build.sh
+++ freefem-docker/build.sh
@@ -1,11 +1,17 @@
-## Build the docker for FreeFem++ compilation
-sudo docker build --network=host -t freefem .
+#!/usr/bin/env bash
 
-## Run the docker
-#sudo docker run -ti freefem
+## Copy the compiled files of FreeFem++
+echo -e "> Extracting ff++-compiled from compiled version"
+docker run --rm -d freefem:compilation | xargs -I {} docker cp {}:/usr/freefem /tmp/ff++-compiled
+
+## Build the docker for FreeFem++ compilation
+echo -e "> Building freefem++ Docker image"
+docker build -t freefem .
 
 ## Save the docker
-sudo docker save -o freefem.tar freefem
+echo -e "> Saving the freefem image"
+docker save -o freefem.tar freefem
 
 ## Compress the image
+echo -e "> Compressing the archive"
 gzip freefem.tar
