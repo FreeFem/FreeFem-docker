6ac3e415c5c626d98d3e76ae615f28fd5a9fd0fd
Simon Garnotel <simon.garnotel@gmail.com>
2018-04-09 17:11:23 +0200

    bug fix, one more

--- freefem-docker/build.sh
+++ freefem-docker/build.sh
@@ -2,11 +2,15 @@
 
 ## Copy the compiled files of FreeFem++
 echo -e "> Extracting ff++-compiled from compiled version"
-sudo docker run --rm -d freefem:compilation | sudo xargs -I {} docker cp {}:/usr/freefem /tmp/ff++-compiled
+sudo docker run --rm -d freefem:compilation | sudo xargs -I {} docker cp {}:/usr/freefem ff++-compiled
 
 ## Build the docker for FreeFem++ compilation
 echo -e "> Building freefem++ Docker image"
 sudo docker build --network=host -t freefem .
+
+## Remove compiled files of FreeFem++
+echo -e "> Remove ff++-compiled"
+sudo rm -R ff++-compiled
 
 ## Save the docker
 echo -e "> Saving the freefem image"
