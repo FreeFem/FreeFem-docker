a3d1877b13d570ef7f5003efcb87a035533e5720
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2019-10-15 10:34:32 +0200

    Trying to avoid failure when cleaning.

--- .jenkins/clean.sh
+++ .jenkins/clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo docker container stop $(sudo docker container ls -aq)
+sudo docker container rm $(sudo docker container ls -aq)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -q)
