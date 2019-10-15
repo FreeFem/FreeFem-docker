39cea3246de207609bd06ded498095ae14d2b150
Pierre Jolivet <pierre.jolivet@enseeiht.fr>
2019-10-15 11:29:26 +0200

    No need for this.
    [ci skip]

--- .jenkins/clean.sh
+++ .jenkins/clean.sh
@@ -2,5 +2,4 @@
 
 sudo docker container stop $(sudo docker container ls -aq)
 sudo docker container rm $(sudo docker container ls -aq)
-sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi $(sudo docker images -q)
