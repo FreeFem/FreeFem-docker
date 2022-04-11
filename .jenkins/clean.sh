3d0a34a937ab29b8f2ee6ec25501f01ddfadc749
Simon Garnotel <simon.garnotel@gmail.com>
2022-04-11 17:07:54 +0200

    Update clean.sh

--- .jenkins/clean.sh
+++ .jenkins/clean.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo docker container stop "$(sudo docker container ls -aq)" | true
-sudo docker container rm "$(sudo docker container ls -aq)" | true
-sudo docker rmi "$(sudo docker images -q)" | true
+sudo docker system prune --all --volumes --force
